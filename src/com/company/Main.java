672eee6b43eb3d618b9e05bf8db8deb099978f90
Krzysztof-kody <53033822+Krzysztof-kody@users.noreply.github.com>
2021-02-11 12:20:19 +0100

    klub

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,8 +5,15 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
-        ArrayList<Zawodnik> klub = new ArrayList<>();
-        klub.add(new Zawodnik("Jan", "Nowak", 22, "śpo", 10, 2, 1000, false));
 
+        ArrayList<Uczen> uczniowie = new ArrayList<Uczen>();
+        uczniowie.add(new Uczen("Jan", "Nowak", 1));
+        uczniowie.get(0).informacjeOUczniu();
+        uczniowie.get(0).setKlasa(2);
+        System.out.println();
+        uczniowie.get(0).informacjeOUczniu();
+     /*   ArrayList<Zawodnik> klub = new ArrayList<>();
+        klub.add(new Zawodnik("Jan", "Nowak", 22, "śpo", 10, 2, 1000, false));
+*/
     }
 }
---
+++ src/com/company/Uczen.java
@@ -0,0 +1,64 @@
+package com.company;
+
+public class Uczen {
+
+    private String imie;
+    private String nazwisko;
+    private int klasa;
+    private String ulubionyPrzedmiot;
+
+    public Uczen(String imie, String nazwisko, int klasa) {
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+        setKlasa(klasa);
+    }
+
+    public void informacjeOUczniu(){
+        System.out.println("Imię: " + this.imie);
+        System.out.println("Nazwisko: " + this.nazwisko);
+        System.out.println("Klasa: " + this.klasa);
+        System.out.print("Ulubiony przedmiot: ");
+        if(this.ulubionyPrzedmiot == null){
+            System.out.println("---");
+        }else {
+            System.out.println(this.ulubionyPrzedmiot);
+        }
+    }
+
+    public String getImie() {
+        return imie;
+    }
+
+    public void setImie(String imie) {
+        this.imie = imie;
+    }
+
+    public String getNazwisko() {
+        return nazwisko;
+    }
+
+    public void setNazwisko(String nazwisko) {
+        this.nazwisko = nazwisko;
+    }
+
+    public int getKlasa() {
+        return klasa;
+    }
+
+    public void setKlasa(int klasa) {
+        if(klasa>0 && klasa <=4) {
+            this.klasa = klasa;
+        }
+        else{
+            System.out.println("err");
+        }
+    }
+
+    public String getUlubionyPrzedmiot() {
+        return ulubionyPrzedmiot;
+    }
+
+    public void setUlubionyPrzedmiot(String ulubionyPrzedmiot) {
+        this.ulubionyPrzedmiot = ulubionyPrzedmiot;
+    }
+}
