4598836a4b8b55c26bd176b061cb208189e602f0
Krzysztof-kody <53033822+Krzysztof-kody@users.noreply.github.com>
2021-02-11 12:32:52 +0100

    klub

---
+++ src/com/company/SortowanieBabelkowe.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class SortowanieBabelkowe {
+
+    public static void main(String[] args) {
+        // 0 1 2 3 4 5 6 7 8 9
+        int[] zbior = new int[10];
+
+        for (int i = 0; i < 10; i++){
+            //zbior[i] = Math.random();
+            System.out.println((int)(Math.random()*15 + 15));
+        }
+
+
+    }
+}
---
+++ src/com/company/Test.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Test {
+    public static void main(String[] args) {
+        ArrayList<Uczen> uczniowie = new ArrayList<Uczen>();
+        uczniowie.add(new Uczen("Jan", "Nowak", 1));
+        uczniowie.get(0).informacjeOUczniu();
+        uczniowie.get(0).setKlasa(2);
+        System.out.println();
+        uczniowie.get(0).informacjeOUczniu();
+     /*   ArrayList<Zawodnik> klub = new ArrayList<>();
+        klub.add(new Zawodnik("Jan", "Nowak", 22, "śpo", 10, 2, 1000, false));
+*/
+    }
+}
