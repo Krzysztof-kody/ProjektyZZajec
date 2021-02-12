350311e3a7fa719c1af0a84a0c98c582c7476f3d
Krzysztof-kody <53033822+Krzysztof-kody@users.noreply.github.com>
2021-02-12 14:39:12 +0100

    klub

--- src/com/company/SortowanieBabelkowe.java
+++ src/com/company/SortowanieBabelkowe.java
@@ -9,13 +9,13 @@ public class SortowanieBabelkowe {
         int[] zbior = new int[10];
         for (int i = 0; i < 10; i++){
             zbior[i] = (int)(Math.random()*100 + 100);
-            System.out.println(zbior[i]);
+            System.out.print(zbior[i] + " ");
         }
         System.out.println("------");
         ArrayList<Integer> zbior1 = new ArrayList<>();
         for (int i = 0; i < 10; i++){
             zbior1.add((int)(Math.random()*100 + 100));
-            System.out.println(zbior1.get(i));
+            //System.out.println(zbior1.get(i));
         }
         /*
         1 4 2 5 8 3
@@ -34,15 +34,32 @@ public class SortowanieBabelkowe {
          |o|   |x|
 
          */
-        int c = 0;
 
-        if(zbior[i] > zbior[i+1]){
-            c = zbior[i];
-            zbior[i] = zbior[i+1];
-            zbior[i+1] = c;
+        int c = 0;
+        int czy = 0;
+
+        for(int j = 0; j < 9 ; j++) {
+            for(int i = 0; i < 9-j ; i++) {
+                System.out.print(".");
+                if (zbior[i] > zbior[i + 1]) {
+                    czy++;
+                    c = zbior[i];
+                    zbior[i] = zbior[i + 1];
+                    zbior[i + 1] = c;
+                }
+            }
+            if(czy == 0) break;
+
         }
+        System.out.println();
+
+        for (int i = 0; i < 10; i++){
+            System.out.print(zbior[i] + " ");
+        }
+
 
 
 
     }
 }
+                                    //
