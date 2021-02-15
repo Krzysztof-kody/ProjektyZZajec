d6432d65a93cb66f81340fd93650af197b608cb3
Krzysztof-kody <53033822+Krzysztof-kody@users.noreply.github.com>
2021-02-15 11:35:35 +0100

    ksiazki

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -13,31 +13,40 @@ public class Main {
         biblioteka.add(new Ksiazka("Antygona","Sofokles",37, 400, "żółty"));
         biblioteka.add(new Ksiazka("Harry Potter","J.K. Rowling",437, 1997, "zielony"));
 
-
-        //biblioteka.get(0).fiszka();
-
+        int wybor = -1;
         Ksiazka ksiazkaTmp = new Ksiazka();
-        System.out.println("---| DODAWANIE KSIAZKI |------------");
-        System.out.println("Tytul ksiazki: ");
-        ksiazkaTmp.setTytul(sc.nextLine());
-        System.out.println("Autor:  ");
-        ksiazkaTmp.setAutor(sc.nextLine());
-        System.out.println("Ilosc stron: ");
-        ksiazkaTmp.setIloscStron(sc.nextInt());
-        System.out.println("Rok wydania: ");
-        ksiazkaTmp.setRokWydania(sc.nextInt());
-        System.out.println("Kolor:  ");
-        ksiazkaTmp.setKolor(sc.next());
 
-        biblioteka.add(ksiazkaTmp);
-
-        for(Ksiazka k: biblioteka){
-            k.fiszka();
+        while(wybor!=0){
+            System.out.println("1. dodaj ksiazke");
+            System.out.println("2. wypisz ksiegozbior");
+            System.out.println("0. zakoncz program");
+            System.out.print(":");
+            wybor = sc.nextInt();
+            switch (wybor){
+                case 1:
+                    System.out.println("---| DODAWANIE KSIAZKI |------------");
+                    System.out.println("Tytul ksiazki: ");
+                    ksiazkaTmp.setTytul(sc.nextLine());
+                    System.out.println("Autor:  ");
+                    ksiazkaTmp.setAutor(sc.nextLine());
+                    System.out.println("Ilosc stron: ");
+                    ksiazkaTmp.setIloscStron(sc.nextInt());
+                    System.out.println("Rok wydania: ");
+                    ksiazkaTmp.setRokWydania(sc.nextInt());
+                    System.out.println("Kolor:  ");
+                    ksiazkaTmp.setKolor(sc.next());
+                    biblioteka.add(ksiazkaTmp);
+                    break;
+                case 2:
+                    for(Ksiazka k: biblioteka){
+                        k.fiszka();
+                    }
+                    break;
+                case 0: break;
+                default:
+                    System.out.println("nie rozumien :(");
+            }
         }
-
-
-
-
 
         /*      ArrayList<Uczen> uczniowie = new ArrayList<Uczen>();
         uczniowie.add(new Uczen("Jan", "Nowak", 1));
