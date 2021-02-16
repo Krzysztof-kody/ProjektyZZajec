cc96d1d3bfc59eb19734d30e01d8a36ee161c107
Krzysztof-kody <53033822+Krzysztof-kody@users.noreply.github.com>
2021-02-16 11:30:04 +0100

    szkola

--- src/com/company/Szkola.java
+++ src/com/company/Szkola.java
@@ -1,11 +1,13 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Szkola {
     public static void main(String[] args) {
 
         ArrayList<Uczen> uczniowie = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
 
         uczniowie.add(new Uczen("Jan", "Nowak", 1));
         uczniowie.add(new Uczen("Janina", "Kowalska", 2));
@@ -13,9 +15,17 @@ public class Szkola {
       //  uczniowie.get(0).informacjeOUczniu();
       //  uczniowie.get(1).informacjeOUczniu();
 
+
         int wybor = -1;
         while (wybor != 0) {
 
+            System.out.println(" --| MENU |-------");
+            System.out.println(" 1. wypisz informacje o uczniach");
+            System.out.println("");
+            System.out.println(" 0. zakoncz dzialanie aplikacji");
+
+
+            wybor = sc.nextInt();
             switch (wybor) {
                 case 1:
                     for (Uczen u : uczniowie) {
