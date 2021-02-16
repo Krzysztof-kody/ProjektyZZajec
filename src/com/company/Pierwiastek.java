79770fcafddd5578a3aa2d0e9c53eb69ea5d5d1e
Krzysztof-kody <53033822+Krzysztof-kody@users.noreply.github.com>
2021-02-16 15:21:42 +0100

    pierwiastek

--- src/com/company/Pierwiastek.java
+++ src/com/company/Pierwiastek.java
@@ -4,6 +4,19 @@ import java.util.Scanner;
 
 public class Pierwiastek {
 
+    public static double pierw3(double a, double b, double x, int n){
+        double eps = 0.0001;
+        double sr =(a+b)/2;
+        double pot = 1;
+        if((b - a) < eps) return sr;
+        for(int i = 0 ; i < n; i++)
+            pot *= sr;
+        if (pot < x) return pierw3(sr,b,x,n);
+        return pierw3(a,sr,x,n);
+
+    }
+
+
     public static double pierw2(double a, double b, double x){
         double eps = 0.0001;
         double sr =(a+b)/2;
@@ -15,16 +28,23 @@ public class Pierwiastek {
     }
     public static double pierw(double x){
         if(x<1)
-            return pierw2(x, 1, x);
+            return pierw3(x, 1, x,2);
         else
-            return pierw2(0, x, x);
+            return pierw3(0, x, x, 2);
+    }
+
+    public static double pierw(double x, int n){
+        if(x<1)
+            return pierw3(x, 1, x,n);
+        else
+            return pierw3(0, x, x, n);
     }
 
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
         double x = sc.nextDouble();
-        System.out.println(pierw(x));
+        System.out.println(pierw(x,3));
 
         /*
         double eps = 0.0001;
