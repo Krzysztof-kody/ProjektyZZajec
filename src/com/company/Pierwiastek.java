bd93f79f414ebf9a0a1bcf187e9ea40893cc0666
Krzysztof-kody <53033822+Krzysztof-kody@users.noreply.github.com>
2021-02-16 15:09:56 +0100

    pierwiastek

--- src/com/company/Pierwiastek.java
+++ src/com/company/Pierwiastek.java
@@ -3,10 +3,30 @@ package com.company;
 import java.util.Scanner;
 
 public class Pierwiastek {
+
+    public static double pierw2(double a, double b, double x){
+        double eps = 0.0001;
+        double sr =(a+b)/2;
+
+        if((b - a) < eps) return sr;
+        if ((sr * sr) < x) return pierw2(sr,b,x);
+        return pierw2(a,sr,x);
+
+    }
+    public static double pierw(double x){
+        if(x<1)
+            return pierw2(x, 1, x);
+        else
+            return pierw2(0, x, x);
+    }
+
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
         double x = sc.nextDouble();
+        System.out.println(pierw(x));
+
+        /*
         double eps = 0.0001;
 
         double a = 0, b = x;
@@ -20,5 +40,7 @@ public class Pierwiastek {
             sr = (a + b)/2.0;
         }
         System.out.println(sr);
+
+         */
     }
 }
