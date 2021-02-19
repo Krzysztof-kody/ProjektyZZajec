42939875c38cb2795eb32d481ee5639d3465ed1e
Krzysztof-kody <53033822+Krzysztof-kody@users.noreply.github.com>
2021-02-19 08:12:45 +0100

    sort przez wst

--- src/com/company/SortowaniePrzezWstawianie.java
+++ src/com/company/SortowaniePrzezWstawianie.java
@@ -8,17 +8,14 @@ public class SortowaniePrzezWstawianie {
 
     public static void main(String[] args) {
         int[] zbior = new int[10];
-        int a;
         int j;
         int c;
 
         for (int i = 0; i < 10; i++){
-            a = (int)(Math.random()*100 + 100);
-            System.out.print(a + " ");
+            zbior[i] = (int)(Math.random()*100 + 100);
+            System.out.print(zbior[i] + " ");
             j = i;
-            zbior[i] = a;
             while(j > 0){
-
                 if(zbior[j] >= zbior[j-1])
                     break;
                 c = zbior[j-1];
