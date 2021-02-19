582761635d63d4ddb2b10493fde47a35a60ee753
Krzysztof-kody <53033822+Krzysztof-kody@users.noreply.github.com>
2021-02-19 14:28:00 +0100

    sort przez wst

---
+++ src/com/company/SitoErastotenesa.java
@@ -0,0 +1,31 @@
+package com.company;
+
+public class SitoErastotenesa {
+
+
+    public static void main(String[] args){
+
+        boolean[] liczby = new boolean[100001];
+        liczby[0] = true;
+        liczby[1] = true;
+
+        for( int n = 2; n < 100001; n++) {
+            if(liczby[n]==false) {
+                for (int i = n + n; i < 100001; i += n) {
+                    liczby[i] = true;
+                }
+            }
+        }
+        int licz = 0;
+        for(int i = 0; i< 100001; i++){
+            if(liczby[i]==false) {
+                System.out.print(i + " ");
+                licz++;
+                if(licz % 20 == 0)
+                    System.out.println();
+            }
+        }
+    }
+}
+
+
