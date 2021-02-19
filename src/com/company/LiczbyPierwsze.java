e4d524769e14b68e75174fe24b574564ff3d37c6
Krzysztof-kody <53033822+Krzysztof-kody@users.noreply.github.com>
2021-02-19 13:55:37 +0100

    sort przez wst

--- src/com/company/LiczbyPierwsze.java
+++ src/com/company/LiczbyPierwsze.java
@@ -1,21 +1,35 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class LiczbyPierwsze {
 
     public static void main(String[] args) {
 
-        int k=30;
 
-        int n = k-1;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Program sprawdzający czy podana liczba jest pierwsza");
+        System.out.println("Podaj liczbę do sprawdzenia: ");
+        int k = sc.nextInt();
+
+        boolean pierwsza = true;
+        int n = (int)Math.sqrt(k);
+
+        // 16
+        // 1 2 4 8 16
 
         while(n > 1){
             if(k % n == 0){
-                System.out.println(".");
-               // break;
+                pierwsza = false;
+                break;
             }
             n--;
         }
-
+        if(pierwsza == true){
+            System.out.println("podana liczba jest pierwsza");
+        }
+        else{
+            System.out.println("podana liczba nie jest pierwsza");
+        }
     }
-
 }
