b4480c0dd644923c6e6d3022f9bba32b6e5c9619
Krzysztof-kody <53033822+Krzysztof-kody@users.noreply.github.com>
2021-02-22 10:57:08 +0100

    for

---
+++ src/com/company/petle/While.java
@@ -0,0 +1,22 @@
+package com.company.petle;
+
+public class While {
+    public static void main(String[] args) {
+
+        int i = 0;
+        while ( i < 10){
+            System.out.println(i);
+            i++;
+        }
+
+        while (1){
+            System.out.println(i);
+            if
+            i++;
+
+        }
+
+
+    }
+
+}
