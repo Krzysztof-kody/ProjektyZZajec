615c5225c5259ebb9ae28fed897a587162a4b581
Krzysztof-kody <53033822+Krzysztof-kody@users.noreply.github.com>
2021-02-23 16:10:42 +0100

    dodawanie do plików

---
+++ src/com/company/petle/ForEach.java
@@ -0,0 +1,23 @@
+package com.company.petle;
+
+public class ForEach {
+    public static void main(String[] args) {
+
+        // stworzona tablica 10cioelementowa intów
+        int[] liczby = new int[10];
+
+        // wypełnienie tablicy wartościami "losowymi" z zakresu 0-10
+        // "przejście" po tablicy za pomoca indeksów
+        for (int i = 0; i < 10; i++) {
+            liczby[i] = (int) (Math.random() * 10);
+        }
+
+        // pętla foreach - "przejdzie" przez cały zbiór liczby wyciągając
+        // do zmiennej liczba kolejne elementy zbioru
+        // dzięki temu możemy wypisywać zmmieniające się wartości
+        // zmiennej liczba
+        for (int liczba : liczby) {
+            System.out.print(liczba + " ");
+        }
+    }
+}
