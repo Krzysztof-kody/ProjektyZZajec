3a3785bde6869264e908e1b8765b4f197353059b
Krzysztof-kody <53033822+Krzysztof-kody@users.noreply.github.com>
2021-02-26 08:39:57 +0100

    sort przez zliczanie

--- src/com/company/sortowanie/SortowaniePrzezZliczanie.java
+++ src/com/company/sortowanie/SortowaniePrzezZliczanie.java
@@ -1,5 +1,27 @@
 package com.company.sortowanie;
 
+/*
+
+5 3 6 2 1 1 3 3
+a
+zbiorWartosci[7]
+
+zbiorWartosci[a] = zbiorWartosci[a] + 1
+zbiorWartosci[a]++
+zbiorWartosci[a] += 1
+
+0
+1 . . (2)
+2 . (1)
+3 . . . (3)
+4
+5 . (1)
+6 . (1)
+
+1 1 2 3 3 3 5 6
+
+ */
+
 public class SortowaniePrzezZliczanie {
     public static void main(String[] args) {
         int[] wartosci = new int[11];
@@ -8,7 +30,7 @@ public class SortowaniePrzezZliczanie {
             tab[i] = (int) (Math.random() * 10);
         }
     /* 0-5
-         3 2 2
+      4 1 1
       0 1 2 3 4 5
       0 0 2 1 0 0
     */
