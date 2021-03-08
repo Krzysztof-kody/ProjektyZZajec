6e566c3bbb1e44ed31c5fc3692cf0a847c4bce15
Krzysztof-kody <53033822+Krzysztof-kody@users.noreply.github.com>
2021-03-08 11:38:23 +0100

    pliki

---
+++ src/com/company/cwiczenia/odczyt.java
@@ -0,0 +1,16 @@
+package com.company.cwiczenia;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class odczyt {
+    public static void main(String[] args) throws FileNotFoundException {
+
+        File plik = new File("src/com/company/cwiczenia/pliki");
+        Scanner sc = new Scanner(plik);
+        while(sc.hasNext()){
+            System.out.println(sc.nextLine());
+        }
+    }
+}
