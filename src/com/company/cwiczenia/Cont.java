2472a10a96aba95a5374848e181af62dcf3948a4
Krzysztof-kody <53033822+Krzysztof-kody@users.noreply.github.com>
2021-03-17 15:14:57 +0100

    \ms

--- src/com/company/cwiczenia/Cont.java
+++ src/com/company/cwiczenia/Cont.java
@@ -3,7 +3,7 @@ package com.company.cwiczenia;
 public class Cont {
     public static void main(String[] s){
         for(int i = 0; i <10; i++) {
-            if( i > 5) continue;
+           if( i > 5) continue;
             System.out.println(i);
         }
     }
--- src/com/company/sortowanie/MS1a2.java
+++ src/com/company/sortowanie/MS1a2.java
@@ -3,10 +3,35 @@ package com.company.sortowanie;
 public class MS1a2 {
 
     public static void main(String[] args){
-        int[] A = {2,4,6,8,1,3,5,7,9};
-        int l1=0, l2=4, p1=3, p2=8;
+        int[] A = {2,4,6,8};
+        int[] B = {0,1,3,3,3};
+        int[] C = new int[9];
 
-
+        int i = 0;
+        int j = 0;
+        for(int k = 0; k < C.length ; k++) {
+            if(i >= A.length){
+                C[k] = B[j];
+                j++;
+                continue;
+            }
+            if(j >= B.length){
+                C[k] = A[i];
+                i++;
+                continue;
+            }
+            if(A[i] < B[j]){
+                C[k] = A[i];
+                i++;
+            }
+            else{
+                C[k] = B[j];
+                j++;
+            }
+        }
+        for(int x: C){
+            System.out.print(x + " ");
+        }
 
     }
 }
