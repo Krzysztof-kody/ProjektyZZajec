16102747a49140b381d3e417b7440ef8fab2b62c
Krzysztof-kody <53033822+Krzysztof-kody@users.noreply.github.com>
2021-03-23 07:49:11 +0100

    \ms

--- src/com/company/cwiczenia/gra1e2/gra.java
+++ src/com/company/cwiczenia/gra1e2/gra.java
@@ -11,6 +11,29 @@ public class gra {
             szczescie = 100;
         }
 
+        public String pasek(int n){
+            String p = "";
+        //    |###                  |
+            int ileHashy = n / 5;
+            int ileSpacji = 20 - ileHashy;
+
+            p += "|";
+            for(int i= 0; i<ileHashy; i++){
+                p+= "#";
+            }
+            for(int i= 0; i<ileSpacji; i++){
+                p+= " ";
+            }
+            p +="|";
+
+            return p;
+        }
+
+        public void statystyki() {
+            System.out.println("Zdro: " + pasek(zdrowie));
+
+            System.out.println("Szcz: " + szczescie);
+        }
     }
     public static void main(String[] args){
         Ludek lu = new Ludek();
