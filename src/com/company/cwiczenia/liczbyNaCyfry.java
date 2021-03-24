c08df64dc4edbea396dcc2410fc12b29de1fe55d
Krzysztof-kody <53033822+Krzysztof-kody@users.noreply.github.com>
2021-03-24 15:16:12 +0100

    a

--- src/com/company/cwiczenia/liczbyNaCyfry.java
+++ src/com/company/cwiczenia/liczbyNaCyfry.java
@@ -1,25 +1,42 @@
 package com.company.cwiczenia;
 
 import java.awt.image.RGBImageFilter;
+import java.util.Scanner;
 
 public class liczbyNaCyfry {
+/*
+       1011
+       10110
+       00001
+ */
+/*
+
+    odczytaj od uzytkownika liczbe calkowita dodatnia/nieujemna
+    odpowedz na pytanie - czy podana liczba jest palindromem?
+
+    12221
+    abba
+
+ */
     public static void main(String[] args){
-        int a = 3223;
-        int b = 0;
-        int dzielnik = 1;
-        /*
-        while(dzielnik < a) {
-            System.out.println((a % (dzielnik * 10)) / dzielnik);
-            dzielnik *= 10;
-        }*/
-
-        while(a>0) {
-            b = b*10 + a%10;
-            System.out.println((a % 10));
-            a = a / 10;
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        String s = "";
+        String c = "";
+        c += a;
+        while(a > 0) {
+            s += (a % 10);
+            a /= 10;
         }
-
-        System.out.println(b);
+        System.out.println(s);
+        System.out.println(c);
+        if(s.equals(c)){
+            System.out.println("palindrom");
+        }
+        else
+        {
+            System.out.println("nie palindrom");
+        }
 
 
     }
