096474a7d24bde4030f923ac851b19b5950e7974
Krzysztof-kody <53033822+Krzysztof-kody@users.noreply.github.com>
2021-03-25 11:23:12 +0100

    a

--- src/com/company/cwiczenia/gra1e2/gra.java
+++ src/com/company/cwiczenia/gra1e2/gra.java
@@ -5,15 +5,22 @@ public class gra {
     static class Ludek{
         int zdrowie;
         int szczescie;
+        int zmeczenie;
+        int czystosc;
+        int najedzenie;
 
-        public Ludek(){
+        String imie;
+        public Ludek(String imie){
             zdrowie = 100;
             szczescie = 100;
+            zmeczenie = 100;
+            czystosc = 100;
+            najedzenie = 60;
+            this.imie = imie;
         }
 
         public String pasek(int n){
             String p = "";
-        //    |###                  |
             int ileHashy = n / 5;
             int ileSpacji = 20 - ileHashy;
 
@@ -30,13 +37,81 @@ public class gra {
         }
 
         public void statystyki() {
-            System.out.println("Zdro: " + pasek(zdrowie));
+            System.out.println("Zdro:  " + pasek(zdrowie));
+            System.out.println("Szcz:  " + pasek(szczescie));
+            System.out.println("Zmecz: " + pasek(zmeczenie));
+            System.out.println("Czyst: " + pasek(czystosc));
+            System.out.println("Najed: " + pasek(najedzenie));
+        }
 
-            System.out.println("Szcz: " + szczescie);
+        public int getZdrowie() {
+            return zdrowie;
+        }
+
+        public void setZdrowie(int zdrowie) {
+            this.zdrowie += zdrowie;
+        }
+
+        public int getSzczescie() {
+            return szczescie;
+        }
+
+        public void setSzczescie(int szczescie) {
+            this.szczescie += szczescie;
+            if(this.szczescie > 100){
+                this.szczescie = 100;
+                setZmeczenie(10);
+                setZdrowie(5);
+            }
+        }
+
+        public int getZmeczenie() {
+            return zmeczenie;
+        }
+
+        public void setZmeczenie(int zmeczenie) {
+            this.zmeczenie += zmeczenie;
+            if(this.zmeczenie > 100){
+                this.zmeczenie = 50;
+                setSzczescie(-20);
+                setZdrowie(-20);
+                setCzystosc(-10);
+            }
+        }
+
+        public int getCzystosc() {
+            return czystosc;
+        }
+
+        public void setCzystosc(int czystosc) {
+            this.czystosc += czystosc;
+        }
+
+        public int getNajedzenie() {
+            return najedzenie;
+        }
+
+        public void setNajedzenie(int najedzenie) {
+            this.najedzenie += najedzenie;
+            if(this.najedzenie>100){
+                System.out.println("PRZEKARMIENIE!!!");
+                this.najedzenie = 100;
+                setZdrowie(-10);
+                setZmeczenie(10);
+                setCzystosc(-10);
+                setSzczescie(10);
+            }
         }
     }
     public static void main(String[] args){
-        Ludek lu = new Ludek();
+        Ludek lu = new Ludek("Czarek");
         lu.statystyki();
+        lu.setNajedzenie(20);
+        lu.statystyki();
+        lu.setNajedzenie(20);
+        lu.statystyki();
+        lu.setNajedzenie(20);
+        lu.statystyki();
+
     }
 }
