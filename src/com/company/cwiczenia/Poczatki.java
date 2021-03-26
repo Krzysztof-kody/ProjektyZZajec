239fb74449477f0d01012ceeab4a773918f7267e
Krzysztof-kody <53033822+Krzysztof-kody@users.noreply.github.com>
2021-03-26 15:59:02 +0100

    a

---
+++ src/com/company/cwiczenia/Poczatki.java
@@ -0,0 +1,18 @@
+package com.company.cwiczenia;
+
+import java.util.Scanner;
+
+public class Poczatki {
+    public static void main(String[] nazwa){
+
+        Scanner sc = new Scanner(System.in);
+        String slowo = "";
+        while(!slowo.equals("exit")){
+            slowo =
+            System.out.println(slowo.toUpperCase());
+        }
+
+
+
+    }
+}
