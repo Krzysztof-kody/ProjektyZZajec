33e9ce31d1d08aa8894a2f60c7866b5ae38dc190
Krzysztof-kody <53033822+Krzysztof-kody@users.noreply.github.com>
2021-03-26 08:42:53 +0100

    a

--- src/com/company/Palindrom.java
+++ src/com/company/Palindrom.java
@@ -2,21 +2,39 @@ package com.company;
 
 public class Palindrom {
 
-    public static void main(String[] args){
-        String s1 = "aaaabbbaaaa";
+    public static boolean czyPalindrom(String s1){
         String s2 = "";
         for(int i = s1.length()-1; i>=0; i--){
             s2 += s1.charAt(i);
         }
-        System.out.println(s1 + " ? " + s1);
         if(s1.equals(s2)){
-            System.out.println("Palindrom");
+            return true;
         }
-        else
-        {
-            System.out.println("Nie palindrom");
+        return false;
+    }
+    public static boolean czyPalindrom1(String s1) {
+
+        for(int i = 0; i < s1.length()/2; i++){
+            if(s1.charAt(i) != s1.charAt(s1.length()-i-1)){
+                return false;
+            }
+        }
+        return true;
+    }
+    /*
+        Wygeneruj 5K palindromów do pliku palindromy.txt.
+        Palindromy mogą sładać się wyłącznie z małych liter alfabetu ang.
+        Długość palindromów od 3 do 20
+
+     */
+
+        public static void main(String[] args){
+        char[] litery = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+
+        for(int i =0; i < 100; i++) {
+            System.out.print( litery[(int)(Math.random() * litery.length)] + " ");
+           }
         }
 
-    }
 
 }
