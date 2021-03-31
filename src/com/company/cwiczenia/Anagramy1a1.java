b05da064bf57e745a44b1afc7a859f3aef307b9b
Krzysztof-kody <53033822+Krzysztof-kody@users.noreply.github.com>
2021-03-31 14:28:03 +0200

    setery

--- src/com/company/cwiczenia/Anagramy1a1.java
+++ src/com/company/cwiczenia/Anagramy1a1.java
@@ -1,14 +1,36 @@
 package com.company.cwiczenia;
 
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Anagramy1a1 {
-    public static void main(String[] args){
-             String s1 = "abdsa";
-             int[] litery = new int[26];
-             for(char l: s1.toCharArray()){
-                 litery[l-'a']++;
-             }
-             for(int i =0; i< litery.length;i++){
-                 System.out.println(i + ": " + litery[i]);
-             }
+    public static boolean anagramy(String s1, String s2){
+        ArrayList<Integer> litery1 = new ArrayList<>(26);
+        ArrayList<Integer> litery2 = new ArrayList<>(26);
+        for (int i = 0; i < 26; i++) {
+            litery1.add(0);
+            litery2.add(0);
+        }
+        for(char l: s1.toCharArray()){
+            litery1.set(l-'a',litery1.get(l-'a')+1);
+        }
+        for(char l: s2.toCharArray()){
+            litery2.set(l-'a',litery2.get(l-'a')+1);
+        }
+        return litery1.equals(litery2);
+    }
+
+    public static boolean anagramy1(String s1, String s2) {
+        char[] ss1 = s1.toCharArray();
+        char[] ss2 = s2.toCharArray();
+        Arrays.sort(ss1);
+        Arrays.sort(ss2);
+        String wynik1 = new String(ss1);
+        String wynik2 = new String(ss2);
+        return wynik1.equals(wynik2);
+    }
+        public static void main(String[] args){
+        System.out.println(anagramy1("abba", "bbaa"));
     }
 }
