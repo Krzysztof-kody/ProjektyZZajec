411b797d6ae907c95a8178a929f277f5ebdfbd03
Krzysztof-kody <53033822+Krzysztof-kody@users.noreply.github.com>
2021-04-07 14:44:24 +0200

    setery

--- src/com/company/cwiczenia/Anagramy1a2.java
+++ src/com/company/cwiczenia/Anagramy1a2.java
@@ -1,31 +1,35 @@
 package com.company.cwiczenia;
 
 public class Anagramy1a2 {
-    public static void main(String[] args){
-        String s1 = "aaasxsxsddd";
-        String s2 = "xxsaaassddd";
 
+    static boolean czyAnagramy(String s1, String s2){
         int[] litery = new int[26];
+        int[] litery2 = new int[26];
+
         for(int i = 0 ; i < s1.length(); i++){
             litery[s1.charAt(i)-'a']++;
         }
-        int[] litery2 = new int[26];
         for(int i = 0 ; i < s2.length(); i++){
             litery2[s2.charAt(i)-97]++;
         }
 
         for (int i = 0; i <litery.length; i++){
             if(litery[i] != litery2[i]){
-                System.out.print("nie ");
-                break;
+                return false;
             }
         }
-        System.out.println("są anagramami");
+        return true;
+    }
 
-        for (int i = 0; i <litery.length; i++) {
-            System.out.println(litery2[i]);
+    public static void main(String[] args){
+        String s1 = "aaasxsxsddd";
+        String s2 = "xxsaaassdcdd";
+
+        if(czyAnagramy(s1,s2)){
+            System.out.println("to anagramy");
         }
-
+        else
+            System.out.println("to nie są anagramy");
 
     }
 }
