2bf80d46b08e233ffb20e228b0e7ffd131167cbc
Krzysztof-kody <53033822+Krzysztof-kody@users.noreply.github.com>
2021-04-08 12:14:06 +0200

    setery

--- src/com/company/cwiczenia/gra1e1/Przedmiot.java
+++ src/com/company/cwiczenia/gra1e1/Przedmiot.java
@@ -1,18 +1,22 @@
 package com.company.cwiczenia.gra1e1;
 
 public class Przedmiot {
+    int ilosc;
     int zdrowie;
     int sytosc;
     int pecherz;
     int zabawa;
+    int cena;
     String nazwa;
 
-    public Przedmiot(String nazwa, int zdrowie, int sytosc, int pecherz, int zabawa){
+    public Przedmiot(String nazwa, int zdrowie, int sytosc, int pecherz, int zabawa, int cena){
         this.nazwa = nazwa;
         this.zdrowie = zdrowie;
         this.pecherz = pecherz;
         this.sytosc = sytosc;
         this.zabawa = zabawa;
+        this.cena = cena;
+        ilosc = 1;
     }
     public void uzyj(Stworek S){
         System.out.println(S.imie + " + " + nazwa);
@@ -20,6 +24,7 @@ public class Przedmiot {
         S.setSytosc(sytosc);
         S.setZdrowie(zdrowie);
         S.setZabawa(zabawa);
+        ilosc--;
     }
 
 
--- src/com/company/cwiczenia/gra1e1/gra.java
+++ src/com/company/cwiczenia/gra1e1/gra.java
@@ -1,31 +1,58 @@
 package com.company.cwiczenia.gra1e1;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class gra {
 
 
     public static void main(String[] args){
         Stworek st = new Stworek("Kulek");
-        st.statystyki();
-        ArrayList<Przedmiot> apteczki = new ArrayList<>();
-        apteczki.add(new Przedmiot("apteczka",50,0,0,0));
-        Przedmiot pilka = new Przedmiot("pilka", 0,0,0,30);
+        Scanner sc = new Scanner(System.in);
+        int kasa = 60;
+        int wybor;
+        Przedmiot apteczka = new Przedmiot("apteczka",50,0,0,0, 50);
+        Przedmiot pilka = new Przedmiot("pilka", 0,0,0,30, 10);
+        Przedmiot marchewka = new Przedmiot("marchewka", 5,15,10,0, 5);
 
         while(st.zywy) {
-            if(st.getZdrowie() < 50){
-                if(apteczki.size() > 0) {
-                    apteczki.get(0).uzyj(st);
-                    apteczki.remove(0);
-                }
-            }else
-            if(st.getZabawa() < 50){
-                    pilka.uzyj(st);
-            }
-            else
-                st.setSytosc(30);
             st.statystyki();
-            System.out.println("---------");
+            System.out.println();
+            System.out.println("1. apteczka");
+            System.out.println("2. pilka");
+            System.out.println("3. marchewka");
+            System.out.print("Wybierz: ");
+            wybor = sc.nextInt();
+
+            switch (wybor) {
+                case 1:
+                    if (apteczka.ilosc > 0) {
+                        if (kasa >= apteczka.cena) {
+                            System.out.println("Uzyto apteczki");
+                            apteczka.uzyj(st);
+                            kasa -= apteczka.cena;
+                        }
+                    }
+                    break;
+                case 2:
+                    if (pilka.ilosc > 0) {
+                        if (kasa >= pilka.cena) {
+                            System.out.println("Uzyto pilka");
+                            pilka.uzyj(st);
+                            kasa -= pilka.cena;
+                        }
+                    }
+                    break;
+                case 3:
+                    if (marchewka.ilosc > 0) {
+                        if (kasa >= marchewka.cena) {
+                            System.out.println("Uzyto marchewka");
+                            marchewka.uzyj(st);
+                            kasa -= marchewka.cena;
+                        }
+                    }
+            }
+
         }
     }
 }
