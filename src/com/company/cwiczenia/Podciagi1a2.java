2ca542000eb076586e53915b8664a9506fe37bb4
Krzysztof-kody <53033822+Krzysztof-kody@users.noreply.github.com>
2021-04-09 09:30:27 +0200

    setery

--- src/com/company/cwiczenia/Podciagi1a2.java
+++ src/com/company/cwiczenia/Podciagi1a2.java
@@ -2,11 +2,12 @@ package com.company.cwiczenia;
 
 public class Podciagi1a2 {
     public static void main(String[] args){
-        String s = "sajgsjdfgjgwiuyewrmbcjjjjjjsadjakhsdkaaaaa";
+        String s = "sajgsjdfgjgwiuyewrmbcjjjjjjsadjakhsdkaaaaaaa";
         int dl = 1;
         int dlMax=0;
+        int posMax = 0;
+        int a = 0;
         for(int i = 1; i < s.length(); i++){
-
             if(s.charAt(i) == s.charAt(i-1)){
                 dl++;
             }
@@ -14,15 +15,20 @@ public class Podciagi1a2 {
             {
                 if(dl>dlMax){
                     dlMax = dl;
+                    posMax = i;
                 }
                 dl = 1;
             }
             if(i+1 == s.length()){
                 if(dl>dlMax){
                     dlMax = dl;
+                    posMax = s.length();
                 }
             }
         }
         System.out.println(dlMax);
+        for(int i = posMax-dlMax; i< posMax; i++){
+            System.out.print(s.charAt(i));
+        }
     }
 }
