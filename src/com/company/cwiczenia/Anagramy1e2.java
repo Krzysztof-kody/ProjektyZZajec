171622c182b4694818ff1d0d77509cee5e5fc3d3
Krzysztof-kody <53033822+Krzysztof-kody@users.noreply.github.com>
2021-04-09 14:16:27 +0200

    setery

--- src/com/company/cwiczenia/Anagramy1e2.java
+++ src/com/company/cwiczenia/Anagramy1e2.java
@@ -1,23 +1,34 @@
 package com.company.cwiczenia;
 
+import java.util.Arrays;
+
 public class Anagramy1e2 {
 
-    public static void main(String[] args){
-     String s1 = "abbacb";
-     String s2 = "bababc";
-    boolean czyAnagram = true;
-     if(s1.length() != s2.length()) czyAnagram = false;
+    static boolean czyAnagramy(String s1, String s2){
+        if(s1.length() != s2.length()) return false;
         for(int i=0; i < s1.length(); i++) {
             s2 = s2.replaceFirst("" + s1.charAt(i), "");
         }
+        if(s2.length() == 0) return true;
+        return false;
+    }
 
-     if(s2.length()==0&&czyAnagram==true){
-         System.out.println("anagramy");
-     }
-     else
-         System.out.println("to nie są anagramy");
+    public static void main(String[] args) {
+        String s1 = "acbabb";
+        String s2 = "ababc";
 
+        char[] ss1 = s1.toCharArray();
+        char[] ss2 = s2.toCharArray();
+        Arrays.sort(ss1);
+        Arrays.sort(ss2);
+        String ws1 = new String(ss1);
+        String ws2 = new String(ss2);
 
+        if(ws1.equals(ws2)){
+            System.out.println("tak");
+        }
+        else
+            System.out.println("nie");
 
     }
-}
+    }
