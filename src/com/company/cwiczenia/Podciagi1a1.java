7bd5301e85dd33bdade077cbd1ac98ab88b8cfc7
Krzysztof-kody <53033822+Krzysztof-kody@users.noreply.github.com>
2021-04-09 08:39:10 +0200

    setery

--- src/com/company/cwiczenia/Podciagi1a1.java
+++ src/com/company/cwiczenia/Podciagi1a1.java
@@ -3,22 +3,30 @@ package com.company.cwiczenia;
 public class Podciagi1a1 {
 
     public static void main(String[] args){
-        String ciag = "lhffffggffffskdfsssssejfksdhfsssssaksjhdaaaaaaah";
+        String ciag = "kdfsssssejhdaaaaaaah";
         int dlMax = 0;
         int dl=0;
         int j=0;
         int posMax=0;
 
         for(int i = 0; i < ciag.length(); i++ ){
-            if(ciag.charAt(i) == ciag.charAt(j))
+            if(i + 1 == ciag.length()){
+                if (dl > dlMax) {
+                    dlMax = dl;
+                    posMax = j;
+                }
+            }
+            else{
+            if(ciag.charAt(i) <= ciag.charAt(i+1))
                 dl++;
             else {
                 if (dl > dlMax) {
                     dlMax = dl;
                     posMax = j;
                 }
-                j = i;
+                j = i+1;
                 dl = 1;
+            }
             }
         }
         System.out.println(dlMax + ", " + posMax);
