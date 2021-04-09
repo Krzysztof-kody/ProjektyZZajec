8eb2cd9e200db3d0a9171183d3ff3497a875526c
Krzysztof-kody <53033822+Krzysztof-kody@users.noreply.github.com>
2021-04-09 14:24:59 +0200

    setery

--- src/com/company/cwiczenia/Anagramy1e2.java
+++ src/com/company/cwiczenia/Anagramy1e2.java
@@ -12,19 +12,21 @@ public class Anagramy1e2 {
         if(s2.length() == 0) return true;
         return false;
     }
-
-    public static void main(String[] args) {
-        String s1 = "acbabb";
-        String s2 = "ababc";
-
+    static boolean czyAnagramy1(String s1, String s2) {
         char[] ss1 = s1.toCharArray();
         char[] ss2 = s2.toCharArray();
         Arrays.sort(ss1);
         Arrays.sort(ss2);
         String ws1 = new String(ss1);
         String ws2 = new String(ss2);
+        return ws1.equals(ws2);
+    }
 
-        if(ws1.equals(ws2)){
+    public static void main(String[] args) {
+        String s1 = "acbabb";
+        String s2 = "ababcb";
+
+        if(czyAnagramy1(s1,s2)){
             System.out.println("tak");
         }
         else
