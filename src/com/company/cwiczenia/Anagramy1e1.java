ae70f0aa68a7c32b5739a049e14df432e589edb6
Krzysztof-kody <53033822+Krzysztof-kody@users.noreply.github.com>
2021-04-13 15:01:09 +0200

    klub

--- src/com/company/cwiczenia/Anagramy1e1.java
+++ src/com/company/cwiczenia/Anagramy1e1.java
@@ -1,17 +1,46 @@
 package com.company.cwiczenia;
 
-public class Anagramy1e1 {
-    public static void main(String[] args){
-// 'a' -> 0
-        int[] ascii = new int[26];
-        String s = "aabbccddaaa";
-        for(int i = 0; i< s.length(); i++){
-            ascii[s.charAt(i) - 97]++;
-        }
-        for(int i=0; i<26; i++){
-            System.out.println( i + ". " + ascii[i]);
-        }
+import java.util.Arrays;
 
+public class Anagramy1e1 {
+
+    static boolean czyAnagramy(String s, String s2){
+        int[] ascii = new int[26];
+        int[] ascii2 = new int[26];
+        if(s.length() != s2.length()) return false;
+        for(int i = 0; i< s.length(); i++){
+            ascii[s.charAt(i) - 'a']++;
+            ascii2[s2.charAt(i) - 'a']++;
+        }
+        for(int i = 0; i < 26; i++){
+            if(ascii[i] != ascii2[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args){
+
+        String s = "aabbccddaaa";
+        String s2 ="aaaaaccddbb";
+
+        char[] sCh = s.toCharArray();
+        char[] s2Ch = s2.toCharArray();
+
+        Arrays.sort(sCh);
+        Arrays.sort(s2Ch);
+
+        String sW = new String(sCh);
+        String s2W = new String(s2Ch);
+
+
+        //if(czyAnagramy(s,s2)){
+        if(sW.equals(s2W)){
+            System.out.println("to sa anagramy");
+        }
+        else
+            System.out.println("to nie są anagramy");
 
     }
 }
