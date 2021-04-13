81c271db99bc157c6105108a92deac4686e455f7
Krzysztof-kody <53033822+Krzysztof-kody@users.noreply.github.com>
2021-04-13 15:37:31 +0200

    klub

--- src/com/company/cwiczenia/Podciag1e1.java
+++ src/com/company/cwiczenia/Podciag1e1.java
@@ -3,6 +3,36 @@ package com.company.cwiczenia;
 public class Podciag1e1 {
 
     public static void main(String[] args){
-        String s = "sajgsjdfgjgwiuyewrmbcjjjjsadjakhsdkaaaaa"
+        String s = "rmbcjjjjsadjakhsdkaaaaax";
+        int j = 0;
+        int dl = 1;
+        int dlMax = 0;
+        int posMax = 0;
+       //  1 1 1 1 4 1 1 1 1 1 1 1 1 1 1 5 1
+
+        for(int i = 0; i < s.length()-1; i++){
+            if(s.charAt(i) == s.charAt(i+1)){
+                dl++;
+            }
+            else {
+                if(dl > dlMax){
+                    dlMax = dl;
+                    posMax = j;
+                }
+                System.out.println(j + " " + dl);
+                dl = 1;
+
+                j = i+1;
+            }
+        }
+        System.out.println(j + " " + dl);
+        if(dl > dlMax) {
+            dlMax = dl;
+            posMax = j;
+        }
+        System.out.println("Max: " + posMax+ ":"+ dlMax);
+        for(){
+
+        }
     }
 }
