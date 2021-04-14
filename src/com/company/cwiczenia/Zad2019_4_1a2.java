22cb979aa01d1d0a3f33ea3d9736dfa514676228
Krzysztof-kody <53033822+Krzysztof-kody@users.noreply.github.com>
2021-04-14 15:17:37 +0200

    Matura 2019 zadanie 4

--- src/com/company/cwiczenia/Zad2019_4_1a2.java
+++ src/com/company/cwiczenia/Zad2019_4_1a2.java
@@ -1,5 +1,9 @@
 package com.company.cwiczenia;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Zad2019_4_1a2 {
 
     static boolean czyPotegaTrojki(int a){
@@ -12,11 +16,15 @@ public class Zad2019_4_1a2 {
         return true;
     }
 
-    public static void main(String[] args){
-        if(czyPotegaTrojki(243))
-            System.out.println("tak");
-        else
-            System.out.println("nie");
+    public static void main(String[] args) throws FileNotFoundException {
+        File plik = new File("src/com/company/cwiczenia/liczby.txt");
+        Scanner in = new Scanner(plik);
 
+        for(int i=0; i< 500; i++) {
+            if (czyPotegaTrojki(in.nextInt()))
+                System.out.println("tak");
+            else
+                System.out.println("nie");
+        }
     }
 }
