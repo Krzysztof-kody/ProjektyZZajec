0d0580456e06ecebda4a85a33297f8795f6091d4
Krzysztof-kody <53033822+Krzysztof-kody@users.noreply.github.com>
2021-04-14 15:11:04 +0200

    Matura 2019 zadanie 4

--- src/com/company/cwiczenia/Zad2019_4_1a2.java
+++ src/com/company/cwiczenia/Zad2019_4_1a2.java
@@ -2,22 +2,21 @@ package com.company.cwiczenia;
 
 public class Zad2019_4_1a2 {
 
-
-
-    public static void main(String[] args){
-        int a = 81;
-czyPotegaTrojki(12365); -> p/f
-
+    static boolean czyPotegaTrojki(int a){
         while(a > 1){
             if(a % 3 == 0)
                 a /=3;
             else
-                break;
+                return false;
         }
-        if(a == 1)
+        return true;
+    }
+
+    public static void main(String[] args){
+        if(czyPotegaTrojki(243))
             System.out.println("tak");
         else
             System.out.println("nie");
-        //System.out.println(a);
+
     }
 }
