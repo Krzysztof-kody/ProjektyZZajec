5a0d0a8f809bb8e61cd4ebdcda18e2f487367727
Krzysztof-kody <53033822+Krzysztof-kody@users.noreply.github.com>
2021-04-16 09:36:49 +0200

    Matura 2019 zadanie 4

--- src/com/company/cwiczenia/Zad2019_4_1a2.java
+++ src/com/company/cwiczenia/Zad2019_4_1a2.java
@@ -16,15 +16,45 @@ public class Zad2019_4_1a2 {
         return true;
     }
 
+    static int silnia(int n){
+        if(n == 0)
+            return 1;
+        return silnia(n-1)*n;
+    }
+    static int liczbaNaCyfry(int n, int[] cyfry){
+
+        int liczbaJednosci;
+        int i = 0;
+        while(n > 0) {
+            liczbaJednosci = n % 10;
+            n /= 10;
+            cyfry[i] = liczbaJednosci;
+            i++;
+        }
+        return i;
+    }
+
     public static void main(String[] args) throws FileNotFoundException {
         File plik = new File("src/com/company/cwiczenia/liczby.txt");
         Scanner in = new Scanner(plik);
-
+        int licznik = 0;
         for(int i=0; i< 500; i++) {
             if (czyPotegaTrojki(in.nextInt()))
-                System.out.println("tak");
-            else
-                System.out.println("nie");
+                licznik++;
+        }
+       // System.out.println(licznik);
+        in.close();
+        in = new Scanner(plik);
+        for(int k=0; k< 500; k++) {
+            int liczba = in.nextInt();
+            int[] tab = new int[6];
+            int i = liczbaNaCyfry(liczba, tab);
+            int suma = 0;
+            for (int j = 0; j < i; j++) {
+                suma += silnia(tab[j]);
+            }
+            if(liczba == suma)
+                System.out.println(liczba + ", " + suma + " : " + (liczba == suma));
         }
     }
 }
