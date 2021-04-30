968fc4c67107d11d2e9c1f8c5ec5c0bf577817fb
Krzysztof-kody <53033822+Krzysztof-kody@users.noreply.github.com>
2021-04-30 08:37:27 +0200

    wstęp do 2d

--- src/com/company/cwiczenia/Zad2017cz4a1.java
+++ src/com/company/cwiczenia/Zad2017cz4a1.java
@@ -2,9 +2,38 @@ package com.company.cwiczenia;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.TreeMap;
 
 public class Zad2017cz4a1 {
+
+
+    public static void cyfropodobne(String a, String b){
+        Map<Character, Integer> C1 = new TreeMap<>();
+        Map<Character, Integer> C2 = new TreeMap<>();
+// 1112 '1'->1 '1'->1 '1'->1 '2'->1 ---> '1'-> 1 '2' ->1
+        for(int i =0; i< a.length(); i++) {
+            C1.put(a.charAt(i), 1);
+        }
+        for(int i =0; i< b.length(); i++) {
+            C2.put(b.charAt(i), 1);
+        }
+        if(C1.equals(C2))
+            System.out.println("tak");
+        else
+            System.out.println("nie");
+/*
+        for(Map.Entry<Character, Integer> i: C2.entrySet()){
+            System.out.println(i);
+        }
+        for(Map.Entry<Character, Integer> i: C1.entrySet()){
+            System.out.println(i.getKey() + " : " + i.getValue());
+        }
+*/
+
+    }
+
 
     public static void main(String[] args) throws FileNotFoundException {
 
@@ -19,13 +48,22 @@ public class Zad2017cz4a1 {
             }
         }
 
+/*
         File plik = new File("src/com/company/cwiczenia/punkty.txt");
         Scanner in = new Scanner(plik);
         int n = 0;
-        for(int i = 0; i <2000; i++){
+        int m = 0;
+        int licznik = 0;
+        for(int i = 0; i <1000; i++){
             n = in.nextInt();
-            System.out.println(n + ": " + (sito[n]==false?"ok":"no"));
+            m = in.nextInt();
+            if(sito[n]==false && sito[m]==false)
+            licznik++;
         }
+        System.out.println(licznik);
     // Zad. dom. Dokończenie zadania 4.1 z matury 2017 czerwiec
+ */
+    cyfropodobne("123","13201");
     }
+
 }
